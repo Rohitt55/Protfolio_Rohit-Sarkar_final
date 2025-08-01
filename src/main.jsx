f2e871588ba24d09d8f14291c9052cd5004bffe8
Rohit Sarkar <rohitt55311@gmail.com>
2025-08-01 13:08:20 +0600

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router";
-import About from './pages/about.jsx';
+import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Home from './pages/Home.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-   <Routes>
-    <Route element ={<App/>}>
-     <Route path="/" element={<Home />} />
-      <Route path='/about' element ={<About />} />
-      <Route path='/project' element ={<Projects/>} />
-    </Route>
-      
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Route>
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function About() {
+const About = () => {
   return (
-    <div>about</div>
+    <div>About</div>
   )
 }
 
