b3109369385e1cf6acd41a1d11fb89f7293103bc
Rohit Sarkar <rohitt55311@gmail.com>
2025-08-01 12:53:27 +0600

    bug solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-
-import { Outlet } from 'react-router'
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'; // Use 'react-router-dom' instead of 'react-router'
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  
-
   return (
     <div className='min-h-screen overflow-hidden bg-black text-white'>
-    <Navbar/>
-     <main className='my-4'>
-      <Outlet/>
-     </main>
-     <Footer/>
+      <Navbar />
+      <main className='my-4'>
+        <Outlet /> {/* This will render the matched child routes */}
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from "react-router";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/about.jsx';
 import Projects from './pages/Projects.jsx';
 import Home from './pages/Home.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-   <Routes>
-    <Route element ={<App/>}>
-     <Route path="/" element={<Home />} />
-      <Route path='/about' element ={<About />} />
-      <Route path='/project' element ={<Projects/>} />
-    </Route>
-      
-    </Routes>
-  </BrowserRouter>,
-)
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Projects />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
